f865052788975b90b5bc6373090f6a5da778faf9
parkgunwook1 <pkw85428@naver.com>
2024-03-27 23:43:07 +0900

    feature:gunwook

---
+++ parkgunwook/src/study/programmers/Day1.java
@@ -0,0 +1,13 @@
+package study.programmers;
+
+public class Day1 {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 13;
+        String val2 = "310";
+
+        System.out.println(msg);
+        System.out.println(val1);
+        System.out.println(val2);
+    }
+}
